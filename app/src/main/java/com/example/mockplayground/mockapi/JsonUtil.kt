8cc24ad0ef6e5de46e2d3f2a9eaad02e6167482f
Robson <robson.v8@hotmail.com>
2024-01-07 17:11:41 -0300

    include some archieves

--- app/src/main/java/com/example/mockplayground/mockapi/JsonUtil.kt
+++ app/src/main/java/com/example/mockplayground/mockapi/JsonUtil.kt
@@ -1,4 +1,20 @@
 package com.example.mockplayground.mockapi
 
+import android.content.Context
+import com.google.gson.Gson
+
 object JsonUtil {
+    inline fun <reified T>getJsonDatafromAsset(context: Context, fileName: String): T? {
+
+        val jsonString: String
+        return try {
+            jsonString = context.assets.open(fileName).bufferedReader().use{ it.readText()}
+
+            Gson().fromJson(jsonString, T::class.java)
+        }catch (ioException: Exception) {
+            ioException.printStackTrace()
+            null
+        }
+    }
+
 }
