c80b5274e4a856246f6b02b9776b98c02a2b4b31
Robson <robson.v8@hotmail.com>
2024-01-07 00:20:35 -0300

    fix gradle problems

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.mockplayground"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mockplayground"
---
+++ app/src/main/java/com/example/mockplayground/mockapi/JsonUtil.kt
@@ -0,0 +1,4 @@
+package com.example.mockplayground.mockapi
+
+object JsonUtil {
+}
